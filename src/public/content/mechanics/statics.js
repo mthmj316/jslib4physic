323973290fb071c116385687469f415027b7f126
mthmj316 <64063837+mthmj316@users.noreply.github.com>
2023-03-09 19:34:46 +0100

    method stub created

--- src/public/content/mechanics/statics.js
+++ src/public/content/mechanics/statics.js
@@ -1,6 +1,10 @@
 module.exports = {
 	
-	
-	
-	
+	/**
+	 * Adds the passed parallel forces (forces with the same line of action)
+	 * All forces need to be in the same scale
+	 */
+	addParallelForces: function(... forces) {
+		
+	}
 }
